61898484a24121a53f27444d7320669805991746
tomasferro-dev <tomasferro.dev>
2025-03-24 18:13:44 -0300

    kata 3

--- kata3/src/Libro.java
+++ kata3/src/Libro.java
@@ -1,2 +1,37 @@
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        while (true) {
+            if (anioPublicacion <= 1900) {
+                System.out.println("Año invalido, debe ser mayor a 1900");
+            } else {
+                this.anioPublicacion = anioPublicacion;
+                break;
+            }
+        }
+
+    }
 }
--- kata3/src/Main.java
+++ kata3/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Libro libro = new Libro();
+
+        libro.setAutor("Edgar Allan Poe");
+        libro.setTitulo("El cuervo");
+        libro.setAnioPublicacion(2010);
+
+        System.out.println("informacion del libro: " + libro.getTitulo() + " " +libro.getAutor() + " " + libro.getAnioPublicacion());
 
     }
 }
